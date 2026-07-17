88cb65f047025a0e940b1abc460d3ed35ff50108
adibabintesiddik-cyber <adibabintesiddik@iut-dhaka.edu>
2026-07-17 23:57:21 +0600

    Completed feature002

--- modules/course.h
+++ modules/course.h
@@ -12,16 +12,3 @@ Course createCourse(char code[], char name[], double credit);
  void viewCourse(Course course);
 
  #endif
-#define COURSE_H
-
-typedef struct Course
-{
-    char code[16];
-    char name[100];
-    double credit;
-} Course;
-
-Course createCourse(char code[], char name[], double credit);
-void viewCourse(Course course);
-
-#endif
