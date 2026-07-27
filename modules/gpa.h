0939e0971b64e51fc638b62d3be4362c4bffd3ad
adibabintesiddik-cyber <adibabintesiddik@iut-dhaka.edu>
2026-07-27 23:59:40 +0600

    Declared required GPA calculator

--- modules/gpa.h
+++ modules/gpa.h
@@ -13,4 +13,4 @@ double calculateGPA(CourseResult results[], int n_results);
 double calculateRequiredGPA(double current_cgpa, double completed_credits,
                              double target_cgpa, double remaining_credits);
 
-#endifs
+#endif
